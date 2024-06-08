c86212f2c28d0cbd8fc61d9a4b15340cd4c323e9
zeroking0429 <k36379694@gmail.com>
2024-06-08 16:54:55 +0900

    Variable and Input

---
+++ input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	printf("Input Data : ");
+	scanf("%d", &n);
+	printf("%d * 3 = %d", n, n * 3);
+
+	return 0;
+}
---
+++ variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int i = 5;
+	float f = 3.141592;
+	char c = 'A';
+
+	printf("int : %d\nfloat : %lf\nchar : %c", i, f, c);
+
+	return 0;
+}
